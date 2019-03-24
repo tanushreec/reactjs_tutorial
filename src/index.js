1352b3ae7cd3a8c490fc0e485c7bdfcbcc16e206
Tanushree Chakravarty <tanushree.vawsum@gmail.com>
2019-03-25 00:52:11 +0530

    Commit#6 - Game History

--- src/index.js
+++ src/index.js
@@ -14,52 +14,17 @@ function Square(props){
   }
   
   class Board extends React.Component {
-
-    constructor(props)
-    {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      }
-    }
-
-    handleClick(i)
-    {
-      const squares = this.state.squares.slice(); //Why slice? (creating a copy of the array) -Immutability
-
-      if(calculateWinner(squares) || squares[i])
-      {
-        return;
-      }
-      
-      squares[i] =  this.state.xIsNext ? 'X' : 0;
-
-      this.setState({squares : squares, xIsNext: !this.state.xIsNext});
-    }
     renderSquare(i) {
       return (<Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}  
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}  
       />
       );
     }
   
-    render() {
-      const winner = calculateWinner(this.state.squares);
-
-      let status;
-
-      if(winner){
-        status = 'Winner:' + winner;
-      }
-      else{
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-        
+    render() {        
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -81,15 +46,84 @@ function Square(props){
   }
   
   class Game extends React.Component {
+    constructor(props)
+    {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(0).fill(null),
+        }],
+        xIsNext: true,
+        stepNumber: 0,
+      }
+    }
+
+    handleClick(i)
+    {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1); //Throwing away future history if we move back
+      const current = history[history.length - 1];
+      const squares = current.squares.slice(); //Why slice? (creating a copy of the array) -Immutability
+
+      if(calculateWinner(squares) || squares[i])
+      {
+        return;
+      }
+
+      squares[i] =  this.state.xIsNext ? 'X' : 'O';
+
+      //concat() doesn't mutate original array.
+      this.setState({history: history.concat([{
+        squares: squares
+      }]), 
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    });
+    }
+
+    jumpTo(step)
+    {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      })
+    }
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return(
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>
+            {desc}
+            </button>
+          </li>
+        );
+      });
+
+      let status;
+
+      if(winner){
+        status = 'Winner:' + winner;
+      }
+      else{
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)} />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
